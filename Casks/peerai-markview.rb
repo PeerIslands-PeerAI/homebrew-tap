d8a198cffbf91b38be50ed92289b1a1aa595a90d
rrajesh1979 <rrajesh1979@gmail.com>
2026-01-16 10:57:58 -0600

    🍺 Add Homebrew cask for PeerAI MarkView v0.1.0
    - Desktop app for viewing and annotating markdown files
    - Supports macOS 10.15+ (aarch64)
    - Installs via: brew tap PeerIslands-PeerAI/tap && brew install --cask peerai-markview

---
+++ Casks/peerai-markview.rb
@@ -0,0 +1,17 @@
+cask "peerai-markview" do
+  version "0.1.0"
+  sha256 "5c54fd6e386f2e66d55fa15c77c68440ef57b7adcc8588187961051f8e68d190"
+
+  url "https://github.com/PeerIslands-PeerAI/peerai-artifact-viewer/releases/download/v#{version}/PeerAI.MarkView_#{version}_aarch64.dmg"
+  name "PeerAI MarkView"
+  desc "Desktop app for viewing and annotating markdown files"
+  homepage "https://github.com/PeerIslands-PeerAI/peerai-artifact-viewer"
+
+  app "PeerAI MarkView.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.peerai.markview",
+    "~/Library/Caches/com.peerai.markview",
+    "~/Library/Preferences/com.peerai.markview.plist",
+  ]
+end
