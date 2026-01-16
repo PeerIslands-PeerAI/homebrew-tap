ea0ec3bc0c2891320eaca145c5731a3acca76363
rrajesh1979 <rrajesh1979@gmail.com>
2026-01-16 11:04:22 -0600

    🔄 Update cask to use homebrew-tap releases
    Move DMG hosting from peerai-artifact-viewer to homebrew-tap
    to allow source repo to remain private

--- Casks/peerai-markview.rb
+++ Casks/peerai-markview.rb
@@ -2,7 +2,7 @@ cask "peerai-markview" do
   version "0.1.0"
   sha256 "5c54fd6e386f2e66d55fa15c77c68440ef57b7adcc8588187961051f8e68d190"
 
-  url "https://github.com/PeerIslands-PeerAI/peerai-artifact-viewer/releases/download/v#{version}/PeerAI.MarkView_#{version}_aarch64.dmg"
+  url "https://github.com/PeerIslands-PeerAI/homebrew-tap/releases/download/v#{version}/PeerAI.MarkView_#{version}_aarch64.dmg"
   name "PeerAI MarkView"
   desc "Desktop app for viewing and annotating markdown files"
   homepage "https://github.com/PeerIslands-PeerAI/peerai-artifact-viewer"
